6fbb9492994a67ea0f916a1fc69632a6c073d708
Ajeet Kumar <ajeet1270@gmail.com>
2022-10-22 20:23:42 +0530

    link

--- Practice/Prog/socketProg/echoClient.java
+++ Practice/Prog/socketProg/echoClient.java
@@ -7,15 +7,16 @@ public class echoClient {
         try{
             System.out.println();
             System.out.println("Client Started!");
-            Socket soc = new Socket("localhost", 3128);
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Enter a String : ");
-            String str = userInput.readLine();
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-            out.println(str);
+            try (Socket soc = new Socket("localhost", 3128)) {
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Enter a String : ");
+                String str = userInput.readLine();
+                PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                out.println(str);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            System.out.println(in.readLine());
+                BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                System.out.println(in.readLine());
+            }
         }
 
         catch(Exception e){
--- Practice/Prog/socketProg/echoServer.java
+++ Practice/Prog/socketProg/echoServer.java
@@ -12,14 +12,15 @@ public class echoServer {
         try{
             System.out.println();
             System.out.println("Waiting for the Connection...");
-            ServerSocket ss = new ServerSocket(3128);
-            Socket soc = ss.accept();
-            System.out.println("Connection Established!!");
-            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-            String str = in.readLine();
-            System.out.println("Client Request : " + str);
-            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-            out.println("Server Say : " + str);
+            try (ServerSocket ss = new ServerSocket(3128)) {
+                Socket soc = ss.accept();
+                System.out.println("Connection Established!!");
+                BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+                String str = in.readLine();
+                System.out.println("Client Request : " + str);
+                PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+                out.println("Server Say : " + str);
+            }
 
         }
 
